08c93aa5e452e8fc97d86098319cad22604c8861
kat-lev <kat@katleverton.com>
2025-04-26 19:34:29 +0200

    Update login form and add texts to json files

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,19 +2,37 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import styles from "./Login.module.css";
+import { useTranslation } from "react-i18next";
 
 export default function Login() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Header />
       <main className={styles.loginPageContainer}>
         <form className={styles.loginForm}>
-          <h1>Login</h1>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-          <button type="submit">Login</button>
+          <h1>{t("login.title")}</h1>
+
+          <label htmlFor="email">{t("login.emailLabel")}</label>
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            required 
+            placeholder={t("login.emailPlaceholder")} 
+          />
+          
+          <label htmlFor="password">{t("login.passwordLabel")}</label>
+          <input 
+            type="password" 
+            id="password" 
+            name="password" 
+            required 
+            placeholder={t("login.passwordPlaceholder")} 
+          />
+          
+          <button type="submit">{t("login.button")}</button>
         </form>
       </main>
       <Footer />
