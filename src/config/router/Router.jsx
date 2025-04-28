4b5772c1859464b322cf05b4002c95621ea6ef20
kat-lev <kat@katleverton.com>
2025-04-28 09:03:02 +0200

    Fix: update Router to match page layout

--- src/config/router/Router.jsx
+++ src/config/router/Router.jsx
@@ -3,17 +3,17 @@ import React from "react";
 import UxWriting from "../../pages/ux-writing/UxWriting";
 import Login from "../../pages/login/Login";
 import Registration from "../../pages/registration/Registration";
-import ServiceDetails from "../../pages/service-details/ServiceDetails";
+
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/ux-writing" element={<UxWriting />} />
+        <Route path="/" element={<UxWriting />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
-        <Route path="/service-details" element={<ServiceDetails />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
